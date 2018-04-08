414a6cf25b2a9646b5ba75c14bfdb12db368ea87
hanzeljesheen <hanzel@botleg.com>
2018-04-08 16:16:50 +0530

    generate mnemonic phrase if one not provided

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ const Prompt      = require('./utils/prompt');
 (async () => {
 
   const prompt    = new Prompt(inquirer),
-        mnemonic  = await prompt.simple('Mnemonic Phrase');
+        mnemonic  = await prompt.simple('Mnemonic Phrase (leave empty to generate one)');
+
+  if (mnemonic.trim() === '') {
+    console.log(`Mnemonic Phrase ${chalk.green(bip39.generateMnemonic())}`);
+    console.log(`Save this phrase in a safe place and use it from now on. ${chalk.red('This phrase will never be generated again.')}`);
+    process.exit(0);
+  }
 
   if (!bip39.validateMnemonic(mnemonic)) {
     console.log(chalk.red.bold('✗ Invalid Mnemonic Phrase'));
